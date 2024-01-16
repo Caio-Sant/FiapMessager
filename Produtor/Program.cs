87517c72e4395da60c3f246e62c35672fb9e5f9e
Caio Santiago <caiosantiagoa@hotmail.com>
2024-01-16 16:58:25 -0300

    Criação de Produtor e Consumidor + Conexão com Azure Service Bus.

--- Produtor/Program.cs
+++ Produtor/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using MassTransit;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var configuration = builder.Configuration;
+var conexao = configuration.GetSection("MassTransitAzure")["Conexao"] ?? string.Empty;
+
+builder.Services.AddMassTransit(x =>
+{
+    x.UsingAzureServiceBus((context, cfg) =>
+    {
+        cfg.Host(conexao);
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
