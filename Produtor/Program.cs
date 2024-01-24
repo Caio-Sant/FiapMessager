91bb0033c7da9f0337b2baece597862a257d9083
Caio Santiago <caiosantiagoa@hotmail.com>
2024-01-24 17:51:13 -0300

    Comentando as linhas do Swagger para publicar a aplicação no Azure.

--- Produtor/Program.cs
+++ Produtor/Program.cs
@@ -23,11 +23,11 @@ builder.Services.AddMassTransit(x =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
 
 app.UseHttpsRedirection();
 
